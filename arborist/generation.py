a8e2df26078a8d775a7557b405e94e4267f41cbd
Pixelz22 <tljohnson1225@gmail.com>
2024-03-04 09:03:00 -0500

    actual tree generation now

--- arborist/generation.py
+++ arborist/generation.py
@@ -4,53 +4,28 @@ from copy import deepcopy
 
 # This is a placeholder for now.
 # Real recipes should be loaded from file
-RECIPES: dict[str, tuple[str, str]] = {}
+RECIPES: dict[str, list[tuple[str, str]]] = {}
 
-def generate_all_trees(target_craft: str) -> list[CraftingTree]:
+def basic_tree(target_element: str) -> SimpleCraftingTree:
     """
-    Generates all possible crafting trees that lead to target craft.
-
-    This is horribly inefficient, and creates tons of duplicate data.
-    I'm considering editing the CraftingTree data structure to be able
-    to hold the various possibly recipes inside of it. It would require
-    a complete redesign of the whole data structure, but I think it's
-    gonna benefit us in the long run with both time/space saved and
-    ease of use. It might even help us when we start getting to optimization.
-    :param target_craft:
-    :return:
+    Generates a naive crafting tree for the target element
+    by choosing the first-available recipe at every opportunity.
+    :param target_element: the element to craft
+    :return: the crafting tree
     """
-    completed_trees: list[CraftingTree] = []
-    q: deque[CraftingTree] = deque()
-    q.append(CraftingTree(target_craft))
+    tree = SimpleCraftingTree(target_element)
 
-    while len(q) > 0:
-        tree = q.popleft()
-        changed = False
+    leaves: deque[CraftTreeNode] = deque()
+    leaves.append(tree.root)
+    while len(leaves):
+        node = leaves.popleft()
 
-        for i in range(len(tree.leaves)):
-            node = tree.leaves[i]
-            if node.craft in "Water;Fire;Earth;Wind":
-                i += 1
-                continue
+        recipe = RECIPES[node.element][0]  # Just choosing first-found recipe
+        new1, new2 = node.load_recipe(recipe)
+        if new1:
+            leaves.append(node.ing1)
+        if new2:
+            leaves.append(node.ing2)
 
-            changed = True
-            for recipe in RECIPES[node.craft]:
-                nt = deepcopy(tree)  # Create a copy of the tree
-                nt_node = nt.leaves[i]
-                nt_node.load_recipe(recipe)  # expand the selected node downwards
-                # Update the leaves of the new tree
-                nt.leaves.pop(i)
-                nt.leaves.append(nt_node.ing1)
-                nt.leaves.append(nt_node.ing2)
-
-                q.append(nt)  # Add the new tree onto the deque
-
-            i += 1
-
-        if changed:
-            del tree  # don't need to carry this around
-        else:
-            completed_trees.append(tree)  # tree is finished
-
-    return completed_trees
+    return tree
 
--- arborist/structures.py
+++ arborist/structures.py
@@ -1,49 +1,51 @@
-from copy import deepcopy
-from collections import deque
-
-
 class CraftTreeNode:
-    def __init__(self, craft: str):
-        self.craft = craft
+    def __init__(self, tree, element: str):
+        self.element = element
         self.ing1: CraftTreeNode | None = None
         self.ing2: CraftTreeNode | None = None
+        self.tree = tree  # can't type cast it because SimpleCraftingTree gets declared afterward
 
-    def __deepcopy__(self, memodict={}):
-        cpy = CraftTreeNode(self.craft)
+    def load_recipe(self, recipe: tuple[str, str]) -> tuple[bool, bool]:
+        """
+        Loads the given recipe into this node, and updates the overarching tree.
+        :param recipe: the recipe to load
+        :return: whether the two ingredients are newly generated nodes
+        """
+        self.ing1, new1 = self.tree.register_element(recipe[0])
+        self.ing2, new2 = self.tree.register_element(recipe[1])
+        return new1, new2
+
+    def is_expanded(self) -> bool:
         if self.ing1:
-            cpy.ing1 = deepcopy(self.ing1)
-            cpy.ing2 = deepcopy(self.ing2)
-        return cpy
-
-    def load_recipe(self, recipe: tuple[str, str]):
-        self.ing1 = CraftTreeNode(recipe[0])
-        self.ing2 = CraftTreeNode(recipe[1])
+            return True
+        return False
 
 
-class CraftingTree:
+class SimpleCraftingTree:
 
-    def __init__(self, target_craft: str):
-        self.root = CraftTreeNode(target_craft)
-        self.leaves = [self.root]
-        self.breadcrumbs = set()
-        self.breadcrumbs.add(target_craft)
+    def __init__(self, target_element: str):
+        self.target_element = target_element
+        self.map: dict[str, CraftTreeNode] = {target_element: CraftTreeNode(self, target_element)}
+        self.root = self.map[target_element]
 
-    def __deepcopy__(self):
-        cpy = CraftingTree(self.root.craft)
-        cpy.root = deepcopy(self.root)
-        cpy.leaves = []
-        cpy.breadcrumbs = deepcopy(self.breadcrumbs)
+    def register_element(self, element: str) -> tuple[CraftTreeNode, bool]:
+        """
+        Similar to get_node(), but will register the element if it doesn't
+        already exist. Returns a reference to the node and whether the
+        node was new or not
+        :param element: the element to register in the tree
+        :return: the node representing that element and whether the node was newly generated
+        """
+        new = False
+        if element not in self.map:
+            self.map[element] = CraftTreeNode(self, element)
+            new = True
+        return self.map[element], new
 
-        q: deque[CraftTreeNode] = deque()
-        q.append(cpy.root)
+    def breadcrumbs(self):
+        return self.map.keys()
 
-        # Perform a levelorder traversal of tree to rediscover leaves
-        while len(q):
-            node = q.popleft()
-            if not node.ing1:
-                cpy.leaves.append(node)
-            else:
-                q.append(node.ing1)
-                q.append(node.ing2)
-
-        return cpy
+    def get_node(self, element: str) -> CraftTreeNode | None:
+        if element in self.map:
+            return self.map[element]
+        return None
