5f7683bd037587f38ec55f556d2f7d76b5a6e2c9
Pixelz22 <tljohnson1225@gmail.com>
2024-03-08 16:39:37 -0500

    Allowed librarian to directly access local database

--- librarian.py
+++ librarian.py
@@ -53,31 +53,45 @@ def save_cache():
     log.debug(f"File {path}/{cached_chunk_hash}.json dumped.")
 
 
-def load_chunk(ch: int, data_type="elements", create_new=False) -> None:
+def load_chunk(ch: int, data_type="elements", create_new=False, local=False) -> None:
     """
     Loads the chunk with the given chunk hash into the cache.
     Raises and IndexError if chunk does not exist.
     :param ch: the chunk hash
     :param data_type: the type of chunk to load
     :param create_new: whether the function is allowed to create new chunks
+    :param local: whether to search for a local version of the chunk
     """
     global cache
     global cached_chunk_hash
     global chunk_updated
     global cached_data_type
 
+    rel_path = f"{data_type}/{ch}.json"
+
     if chunk_updated:
         save_cache()
     log.debug(f"Retrieving chunk {ch} (data type={data_type})...")
-    response = session.get(f"{DB_URL}/{data_type}/{ch}.json")
-    if response.status_code == 404:
-        if not create_new:
+
+    if local:  # Searching in local database
+        if os.path.exists(f"{LOCAL_DB_PATH}/{rel_path}"):
+            cache = json.load(open(f"{LOCAL_DB_PATH}/{rel_path}"))
+        elif create_new:
+            cache = {}
+        else:
             raise IndexError(f"Attempted to access non-existent chunk: {data_type}/{ch}.json")
-        log.debug(f"Chunk {ch} (data type={data_type}) does not exist!")
-        cache = {}
-    else:
-        log.debug(f"Chunk {ch} (data type={data_type}) succesfully downloaded!")
-        cache = json.loads(response.content)
+
+    else:  # Searching in remote database
+        response = session.get(f"{DB_URL}/{rel_path}")
+        if response.status_code != 404:
+            log.debug(f"Chunk {ch} (data type={data_type}) successfully downloaded!")
+            cache = json.loads(response.content)
+        elif create_new:
+            log.debug(f"Chunk {ch} (data type={data_type}) does not exist! Creating new chunk...")
+            cache = {}
+        else:
+            raise IndexError(f"Attempted to access non-existent chunk: {data_type}/{ch}.json")
+
     cached_chunk_hash = ch
     cached_data_type = data_type
     chunk_updated = False
@@ -89,7 +103,6 @@ def load_all_data() -> None:
 
     WARNING: After calling this function, you will no longer be able
     to load individual chunks.
-    :return:
     """
     global cache
     global cached_chunk_hash
@@ -102,44 +115,42 @@ def load_all_data() -> None:
     cached_chunk_hash = -2
 
 
-def query_data(key: str, data_type="element") -> dict:
+def query_data(key: str, data_type="element", local=False) -> dict:
     """
     Fetches the data associated with the given key and data type.
-    :param key:
-    :param data_type:
-    :return:
+    :param key: the key to search for
+    :param data_type: the type of the data
+    :param local: whether to use the local database
+    :return: the data stored with the key
     """
     ch = chunk_hash(key)
     if ch != cached_chunk_hash and cached_chunk_hash != -2:
         log.debug(f"Loading chunk {ch} (data type={data_type})...")
-        load_chunk(ch, data_type)
+        load_chunk(ch, data_type, local=local)
 
     return cache[key]
 
 
-def store_data(key: str, data: dict, data_type="element") -> bool:
+def store_data(key: str, data: dict, data_type="element", local=False) -> bool:
     """
     Stores the given data in the database.
     :param key: the key of the data
     :param data: the data to store
     :param data_type: the type of the data
+    :param local: whether to use the local database
     :return: True if the key already existed, false otherwise
     """
     global chunk_updated
 
     ch = chunk_hash(key)
     if ch != cached_chunk_hash and cached_chunk_hash != -2:
-        load_chunk(ch, data_type, create_new=True)
+        load_chunk(ch, data_type, create_new=True, local=local)
 
     chunk_updated = True
-    if key in cache:
-        cache[key] = data
-        log.debug(f"Saved element {key} to chunk hash {ch} successfully. Data saved: {data} (key in cache)")
-        return True
-
-    log.debug(f"Saved element {key} to chunk hash {ch} successfully. Data saved: {data} (key not in cache)")
+    newKey = key not in cache
     cache[key] = data
-    return False
+    log.debug(f"Saved element {key} to chunk hash {ch} successfully. Data saved: {data} (key in cache)")
+    return newKey
 
 
 # Name of element -> run some hashing function on it -> compression function ->
